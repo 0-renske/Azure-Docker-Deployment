93c4300d080b5a1d7161bf1de2761af87331f963
0-renske <527242@student.fontys.nl>
2025-04-14 10:39:02 +0200

    Update index.js
    .

--- index.js
+++ index.js
@@ -1,26 +1,20 @@
 const express = require('express');
 const next = require('next');
-const path = require('path');
 
+const port = process.env.PORT || 3000;
 const dev = process.env.NODE_ENV !== 'production';
+
 const app = next({ dev });
 const handle = app.getRequestHandler();
-
-const port = process.env.PORT || 80;
 
 app.prepare().then(() => {
   const server = express();
 
-  // Serve static files from the "public" folder
-  server.use(express.static(path.join(__dirname, 'public')));
-
-  // Handle all other requests with Next.js's request handler
   server.all('*', (req, res) => {
     return handle(req, res);
   });
 
-  server.listen(port, (err) => {
-    if (err) throw err;
-    console.log(`Server is running on port ${port}`);
+  server.listen(port, () => {
+    console.log(`> Ready on http://localhost:${port}`);
   });
 });
