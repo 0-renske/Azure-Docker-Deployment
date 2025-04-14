7cdca0b859b7b5832ff8f2c15e06494967b29074
0-renske <527242@student.fontys.nl>
2025-04-14 10:45:57 +0200

    Update index.js
    first edit

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const next = require('next');
 
 const port = process.env.PORT || 3000;
 const dev = process.env.NODE_ENV !== 'production';
-
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
