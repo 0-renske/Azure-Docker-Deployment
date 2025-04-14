132c7390a06245bff4acac9bcc472356866c0153
0-renske <527242@student.fontys.nl>
2025-04-14 10:19:12 +0200

    Update index.js
    changed this index.js

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 const express = require('express');
-const app = express();
+const next = require('next');
+const path = require('path');
+
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
 const port = process.env.PORT || 80;
 
+app.prepare().then(() => {
+  const server = express();
 
-app.use(express.static('public'));  
+  // Serve static files from the "public" folder
+  server.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(port, () => {
+  // Handle all other requests with Next.js's request handler
+  server.all('*', (req, res) => {
+    return handle(req, res);
+  });
+
+  server.listen(port, (err) => {
+    if (err) throw err;
     console.log(`Server is running on port ${port}`);
+  });
 });
