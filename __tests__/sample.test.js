29361e3d8bd5de22cb52ea65a58a234d0a2b6524
0-renske <527242@student.fontys.nl>
2025-06-08 13:36:31 +0200

    Create sample.test.js
    ew python

---
+++ __tests__/sample.test.js
@@ -0,0 +1,8 @@
+import { render, screen } from '@testing-library/react';
+
+describe('Sample test', () => {
+  it('displays a heading', () => {
+    render(<h1>Hello from test</h1>);
+    expect(screen.getByText('Hello from test')).toBeInTheDocument();
+  });
+});
